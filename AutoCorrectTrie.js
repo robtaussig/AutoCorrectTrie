5b20c5a52c9e53fcefe1278e0382225b5f1f9c9a
Robert Taussig <robert.taussig@gmail.com>
2017-05-03 23:11:26 -0400

    Sort suggested words by proximity of keys on the keyboard to improve accuracy

--- AutoCorrectTrie.js
+++ AutoCorrectTrie.js
@@ -7,6 +7,18 @@ class Trie {
         this.trie = {};
         this.validWords = validWords;
         this.createNodes();
+        this.closenessMap = [
+            ['q','w','e','r','t','y','u','i','o','p'],
+            ['a','s','d','f','g','h','j','k','l'],
+            ['z','x','c','v','b','n','m']
+        ];
+        this.closenessMap = {
+            a: {x:1,y:0}, b: {x:2,y:4}, c: {x:2,y:2}, d: {x:1,y:2}, e: {x:0,y:2}, 
+            f: {x:1,y:3}, g: {x:1,y:4}, h: {x:1,y:5}, i: {x:0,y:7}, j: {x:1,y:6}, 
+            k: {x:1,y:7}, l: {x:1,y:8}, m: {x:2,y:6}, n: {x:2,y:5}, o: {x:0,y:8}, 
+            p: {x:0,y:9}, q: {x:0,y:0}, r: {x:0,y:3}, s: {x:1,y:1}, t: {x:0,y:4}, 
+            u: {x:0,y:6}, v: {x:2,y:3}, w: {x:0,y:1}, x: {x:2,y:1}, y: {x:0,y:5}, z: {x:2,y:0}
+        };
     }
 
     createNodes() {
@@ -23,7 +35,7 @@ class Trie {
                 this.trie[firstLetter] = currentNode;
             }
             for (let j = 1; j < currentWord.length; j++) {
-                currentNode = currentNode.traverseOrCreateNode(currentWord[j]);
+                currentNode = currentNode.moveToOrCreateNode(currentWord[j]);
             }
             currentNode.setIsWord();
         }
@@ -45,33 +57,54 @@ class Trie {
     }
 
     autoCorrect(word) {
-        if (typeof word !== 'string' || word.length < 1) return 'Input must be a string greater than length 0';
+        if (typeof word !== 'string' || word.length < 1) return [];
         let suggestedWords = [];
         if (this.checkIsWord(word)) {
-            return `${word} is a real word!`;
+            return [word];
         }
         else {
             for (let i = 0; i < word.length; i++) {
+                // Go through each letter in the given word, and check whether replacing it with any other letter results in a valid word
                 let closeWords = this.findRelatives(word, i);
                 suggestedWords = suggestedWords.concat(closeWords);
             }
         }
-        return suggestedWords;
+        return suggestedWords.sort((a,b) => {
+            return this.measureClosesness(a, word) - this.measureClosesness(b, word);
+        });
+    }
+    //Assumes both words are the same length and only deviate by one letter
+    measureClosesness(word1, word2) {
+        let firstUniqueLetter;
+        let secondUniqueLetter;
+        for (let i = 0; i < word1.length; i++) {
+            if (word1[i] !== word2[i]) {
+                firstUniqueLetter = word1[i];
+                secondUniqueLetter = word2[i];
+            }
+        }
+        let xDiff = Math.abs(this.closenessMap[firstUniqueLetter].x - this.closenessMap[secondUniqueLetter].x);
+        let yDiff = Math.abs(this.closenessMap[firstUniqueLetter].y - this.closenessMap[secondUniqueLetter].y);
+        let closeness = Math.sqrt(Math.pow(xDiff,2) + Math.pow(yDiff,2));
+        return closeness;
     }
 
     findRelatives(word, nodePosition) {
         let relatives = [];
         let children = [];
         if (nodePosition === 0) {
+            //Replace first letter with all other valid characters
             for (let firstLetter in this.trie) {
-                if (firstLetter !== word[0]) {
+                if (firstLetter !== word[nodePosition]) {
                     children.push(this.trie[firstLetter]);
                 }
             }
         }
         else {
             let currentNode = this.trie[word[0]];
+            //find node at nodePosition
             for (let i = 1; i < nodePosition; i++ ) {
+                //No legal words left
                 if (!currentNode) {
                     break;
                 }
@@ -85,7 +118,7 @@ class Trie {
                 }
             }   
         }
-
+        //Test whether it is possible to find a valid word by following the same path as the original word, except for the replaced letter
         for (let i = 0; i < children.length; i++) {
             let depth = 1;
             let currentNode = children[i];
@@ -93,7 +126,7 @@ class Trie {
                 currentNode = currentNode.next[word[nodePosition + depth]];
                 depth++;
             }
-            // console.log(nodePosition, depth);
+            //If hit the word length and is a valid word, push into relatives array
             if (nodePosition + depth === word.length && currentNode.checkIsWord()) {
                 relatives.push(currentNode.string);
             }
@@ -116,12 +149,14 @@ class Node {
         }
     }
 
-    traverseOrCreateNode(value) {
+    moveToOrCreateNode(value) {
         let returnValue;
+        //Create new node if it doesn't exist
         if (!this.next[value]) {
             returnValue = new Node(value, this);
             this.next[value] = returnValue;
         }
+        //Otherwise, move to it
         else {
             returnValue = this.next[value];
         }
@@ -137,10 +172,6 @@ class Node {
         }
     }
 
-    scanNext(value) {
-        return !!this.next[value];
-    }
-
     checkIsWord() {
         return this.isWord;
     }
@@ -148,20 +179,10 @@ class Node {
     setIsWord() {
         this.isWord = true;
     }
-
-    closeWords() {
-        let closeWords = [];
-        for (let key in this.next) {
-
-            if (this.next[key].checkIsWord()) {
-                closeWords.push(this.next[key].string);
-            }
-        }
-        return closeWords;
-    }
 }
 
-let trie = new Trie(text);
+module.exports = (word) => {
+    let trie = new Trie(text);
 
-let word = 'zogging';
-console.log(trie.autoCorrect(word));
+    return trie.autoCorrect(word);
+};
