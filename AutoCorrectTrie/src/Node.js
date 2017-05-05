cc0fc28517cde8984d37f507fc279ca2aeb3dc58
Robert Taussig <robert.taussig@gmail.com>
2017-05-04 23:21:52 -0400

    Break up into classes and separate into separate files

---
+++ AutoCorrectTrie/src/Node.js
@@ -0,0 +1,36 @@
+module.exports =  class Node {
+    constructor(value, prev) {
+        this.next = {};
+        this.prev = prev;
+        this.isWord = false;
+        this.value = value.toLowerCase();
+        if (prev) {
+            this.string = prev.string + this.value;
+        }
+        else {
+            this.string = this.value;
+        }
+    }
+
+    moveToOrCreateNode(value) {
+        let returnValue;
+        //Create new node if it doesn't exist
+        if (!this.next[value]) {
+            returnValue = new Node(value, this);
+            this.next[value] = returnValue;
+        }
+        //Otherwise, move to it
+        else {
+            returnValue = this.next[value];
+        }
+        return returnValue;
+    }
+
+    checkIsWord() {
+        return this.isWord;
+    }
+
+    setIsWord() {
+        this.isWord = true;
+    }
+}
